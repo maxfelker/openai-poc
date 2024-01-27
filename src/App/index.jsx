689a8da9e8cc1358e0efb475c063f5fdcf1638ef
MW Felker <mwadefelker@gmail.com>
2024-01-26 23:48:36 -0500

    Stubbed out basic terminal styling and form

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,10 +1,47 @@
+import { useState, useEffect, useCallback } from 'react';
+import styles from './styles.module.css';
+
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [history, setHistory] = useState([]);
+  const [pwd, setPwd] = useState('~ ');
+  const user = "guest@openai.maxfelker.com";
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  const handleFormSubmit = useCallback(function(event) {
+    event.preventDefault();
+    setHistory(prevHistory => [...prevHistory, inputValue]);
+    setInputValue('');
+  }, [setHistory, setInputValue, inputValue]);
+
+  useEffect(function() {
+    function handleEnterPress(event) {
+      if (event.key === 'Enter') {
+        handleFormSubmit(event);
+      }
+    }
+    window.addEventListener('keydown', handleEnterPress);
+    return function cleanup() {
+      window.removeEventListener('keydown', handleEnterPress);
+    };
+  }, [handleFormSubmit]);
 
   return (
     <>
-      <p>guest@openai.maxfelker.com:~</p>
+      <div className={styles.historyList}>
+        {history.map((command, index) => (
+          <p key={index}>{command}</p>
+        ))}
+      </div>
+      <form name="terminal" onSubmit={handleFormSubmit}>
+        <label htmlFor="cli-input">
+          <p>{user}:{pwd}</p>
+        </label>
+        <input name="cli-input" type="text" autoFocus value={inputValue} onChange={handleInputChange} />
+      </form>
     </>
-  )
+  );
 }
-
- App
